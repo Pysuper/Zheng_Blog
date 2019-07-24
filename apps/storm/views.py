0e7e658816d13c57364209c6b903878a85611eea
pysuper <1821764535@qq.com>
2019-07-24 12:15:48 +0800

    调整前端代码

--- apps/storm/views.py
+++ apps/storm/views.py
@@ -18,15 +18,12 @@ from haystack.query import SearchQuerySet
 
 # Create your views here.
 class IndexView(generic.ListView):
-    """
-        首页视图,继承自ListVIew，用于展示从数据库中获取的文章列表
-    """
-    # 获取数据库中的文章列表
-    model = Article
-    # template_name属性用于指定使用哪个模板进行渲染
+    """首页视图,继承自ListVIew，用于展示从数据库中获取的文章列表"""
 
-    # context_object_name属性用于给上下文变量取名（在模板中使用该名字）
-    context_object_name = 'articles'
+    model = Article # 获取数据库中的文章列表
+
+    # template_name = "index.html"# template_name属性用于指定使用哪个模板进行渲染
+    context_object_name = 'articles'    # 用于给上下文变量取名（在模板中使用该名字）
 
     paginate_by = 10
 
